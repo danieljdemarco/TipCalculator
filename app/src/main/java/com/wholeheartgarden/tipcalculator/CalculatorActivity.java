25b4a769fe7c9d252b897d747cf3bc7370ae6cd1
danieldemarco <daniel.j.demarco@gmail.com>
2018-07-20 18:16:51 -0400

    Add comments Cast UI elements Add XML attributes to EditTexts

--- app/src/main/java/com/wholeheartgarden/tipcalculator/CalculatorActivity.java
+++ app/src/main/java/com/wholeheartgarden/tipcalculator/CalculatorActivity.java
@@ -9,27 +9,33 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class CalculatorActivity extends AppCompatActivity implements View.OnFocusChangeListener, View.OnClickListener {
+    private final String TAG = CalculatorActivity.class.getSimpleName();
 
+    // UI components
     private EditText mBillSubtotal;
     private EditText mTipPercent;
     private EditText mTipAmount;
     private Button mRoundDown;
     private Button mRoundUp;
-    private final String TAG = CalculatorActivity.class.getSimpleName();
-    private int mLastEdited;
 
+    // Keeps track of whether the tip amount or tip percent was last edited. When another field
+    // such as the Bill Subtotal is changed, it will update the tip field that was not last
+    // edited, assuming that the user wants to keep the more recently edited field
+    private int mLastEdited;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
 
-        mBillSubtotal = findViewById(R.id.edit_text_bill_subtotal);
-        mTipPercent = findViewById(R.id.edit_text_tip_percent);
-        mTipAmount = findViewById(R.id.edit_text_tip_amount);
-        mRoundDown = findViewById(R.id.button_round_down);
-        mRoundUp = findViewById(R.id.button_round_up);
+        // initialize UI components
+        mBillSubtotal = (EditText) findViewById(R.id.edit_text_bill_subtotal);
+        mTipPercent = (EditText) findViewById(R.id.edit_text_tip_percent);
+        mTipAmount = (EditText) findViewById(R.id.edit_text_tip_amount);
+        mRoundDown = (Button) findViewById(R.id.button_round_down);
+        mRoundUp = (Button) findViewById(R.id.button_round_up);
 
+        // set listeners
         mBillSubtotal.setOnFocusChangeListener(this);
         mTipPercent.setOnFocusChangeListener(this);
         mTipAmount.setOnFocusChangeListener(this);
@@ -38,21 +44,24 @@ public class CalculatorActivity extends AppCompatActivity implements View.OnFocu
 
     }
 
+    // calculates the tip percent based on current tip amount
     private float calculateTipPercent() {
         float billSubtotal = Float.parseFloat(mBillSubtotal.getText().toString());
         float tipAmount = Float.parseFloat(mTipAmount.getText().toString());
         return (tipAmount / billSubtotal) * 100;
     }
 
+    // calculates the tip amount based on current tip percent
     private float calculateTipAmount() {
         float billSubtotal = Float.parseFloat(mBillSubtotal.getText().toString());
         float tipPercent = Float.parseFloat(mTipPercent.getText().toString()) / 100;
         return billSubtotal * tipPercent;
     }
 
+    // whenever the user changes a field, attempt to update the other fields
     @Override
     public void onFocusChange(View view, boolean hasFocus) {
-        if (!hasFocus) {
+        if (!hasFocus) { // look for the field that is losing focus
             switch (view.getId()) {
                 case R.id.edit_text_bill_subtotal:
                     Toast.makeText(this, "Bill Subtotal", Toast.LENGTH_SHORT).show();
@@ -85,6 +94,8 @@ public class CalculatorActivity extends AppCompatActivity implements View.OnFocu
         }
     }
 
+    // handle click events to round the tip amount up or down to the nearest integer, and update
+    // tip percent
     @Override
     public void onClick(View view) {
         double oldTipAmount;
